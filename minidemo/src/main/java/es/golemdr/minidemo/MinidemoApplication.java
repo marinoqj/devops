49a0137f9524f86c84f4ff15b1e6e971bb2abc5b
marino <marino.quijada@golemdr.es>
2024-09-21 00:56:37 +0200

    Cambios para prueba de Sonar

--- minidemo/src/main/java/es/golemdr/minidemo/MinidemoApplication.java
+++ minidemo/src/main/java/es/golemdr/minidemo/MinidemoApplication.java
@@ -1,5 +1,6 @@
 package es.golemdr.minidemo;
 
+import java.io.*
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,11 +8,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MinidemoApplication {
 
 	public static void main(String[] args) {
+		
+		private String noSeUsa = null;
+		
 		SpringApplication.run(MinidemoApplication.class, args);
 		
 		System.out.println("#######################                                       #######################");
 		System.out.println("####################### Minidemo funcionando correctamente... #######################");
 		System.out.println("#######################                                       #######################");
+		
+		
+		
 	}
 
 }
